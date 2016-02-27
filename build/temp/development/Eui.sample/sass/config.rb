479c15f093fda475722a626fd4440963b50d43ea
benney <benneykwag@gmail.com>
2016-02-27 17:52:28 +0900

    --

--- build/temp/development/Eui.sample/sass/config.rb
+++ build/temp/development/Eui.sample/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('../../../../../eui-sample/sass/config.rb')
-cache_path = '/Volumes/Disk2/git.projects/senproro/src/main/webapp/eui-workspace/build/.sass-cache'
+cache_path = '/Volumes/Disk2/git.projects/eui-workspace/build/.sass-cache'
--- build/temp/development/Eui.sample/sencha-compiler/app/full-page-master-bundle.js
+++ build/temp/development/Eui.sample/sencha-compiler/app/full-page-master-bundle.js
@@ -1,2 +1,2 @@
 // @tag full-page
-// @require /Volumes/Disk2/git.projects/senproro/src/main/webapp/eui-workspace/eui-sample/app.js
+// @require /Volumes/Disk2/git.projects/eui-workspace/eui-sample/app.js
