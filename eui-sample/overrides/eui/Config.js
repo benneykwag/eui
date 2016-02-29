817a5c764cddd7621b61f8b77703318566a989cf
benney <benneykwag@gmail.com>
2016-02-29 13:20:31 +0900

    Cofig add

---
+++ eui-sample/overrides/eui/Config.js
@@ -0,0 +1,13 @@
+/***
+ * App전역 변수 설정.
+ */
+Ext.define('Override.eui.Config', {
+    override: 'eui.Config',
+    localeUrl : 'resources/data/i18n.json',
+
+    data: {
+        message : [
+            {"MSG_ID": "F000000119", "MSG_LABEL": "신청일자를 입력해 주세요.zz"}
+        ]
+    }
+});
