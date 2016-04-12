3933197f4e13db10e03fdd8fbdcb345502553e4f
benney <benneykwag@gmail.com>
2016-04-12 17:03:14 +0900

    update app

---
+++ eui-sample/app/model/Message.js
@@ -0,0 +1,55 @@
+Ext.define('Eui.sample.model.Message', {
+    extend: 'Eui.sample.model.Base',
+    identifier: {
+        type: 'sequential',
+        prefix: 'MSG'
+    },
+    fields: [
+
+    ],
+
+    proxy: {
+        type: 'ajax',
+        actionMethods: {
+            read: 'GET',
+            create: 'POST',
+            update: 'POST',
+            destroy: 'POST'
+        },
+
+        api: {
+            create: 'resources/data/mygriddata.json?create',
+            read: 'resources/data/mygriddata.json?read',
+            update: 'resources/data/mygriddata.json?update',
+            destroy: 'resources/data/mygriddata.json?destroy'
+        },
+        reader: {
+            type: 'json',
+            rootProperty: 'data'
+        },
+        writer: {
+            type: 'json',
+            allowSingle: false,
+            writeAllFields: true
+        },
+        listeners: {
+            exception: function (proxy, response, operation) {
+                var json = Ext.decode(response.responseText);
+                if (json) {
+                    Ext.MessageBox.show({
+                        title: 'Save Failed',
+                        msg: json.message,
+                        icon: Ext.MessageBox.ERROR,
+                        buttons: Ext.Msg.OK
+                    });
+                } else
+                    Ext.MessageBox.show({
+                        title: 'EXCEPTION',
+                        msg: operation.getError(),
+                        icon: Ext.MessageBox.ERROR,
+                        buttons: Ext.Msg.OK
+                    });
+            }
+        }
+    }
+});
