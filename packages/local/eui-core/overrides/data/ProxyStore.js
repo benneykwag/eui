29b3edf661bba98938150bbc4521deef406b65fa
benney <benneykwag@gmail.com>
2016-04-12 20:09:50 +0900

    store의 sync 전에 모델 validation을 알려주도록 수정.

---
+++ packages/local/eui-core/overrides/data/ProxyStore.js
@@ -0,0 +1,29 @@
+Ext.define('Override.data.ProxyStore', {
+    override: 'Ext.data.ProxyStore',
+
+    /***
+     * 모델 validat가 false인 경우 메시지를 호출해 알린다.
+     */
+    recordsValidationCheck: function () {
+        var source = this.getDataSource(),
+            items = source.items,
+            len = items.length,
+            i;
+
+        for (i = 0; i < len; i++) {
+            if (!items[i].recordValidationCheck()) {
+                break;
+            }
+        }
+    },
+
+    /***
+     * sync전에 레코드를 미리 확인한다.
+     * @param option
+     * @returns {*}
+     */
+    checkSync: function (option) {
+        this.recordsValidationCheck();
+        return this.sync(option);
+    }
+});
