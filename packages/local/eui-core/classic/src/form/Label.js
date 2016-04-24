16c417d3bdf672f2eb8d7bb1ac5e2f496a246577
benney <benneykwag@gmail.com>
2016-04-24 18:16:27 +0900

    Eui.sample.view.grid.Basic클래스를 이용 등록 버튼을 클릭하면 입력용 폼이 호출된다. 메시지 내용을 입력하면 formBind되어 최초 disabled된 "저장"버튼이 활성화 되어야하나 간혹 활성화 되지 않음. 이를 위해 Eui.form.Panel클래스는 아래와 같이 afterrender이후 isValid메소드를 호출하게 되어 있음. 필스 표시처리 closes #5

--- packages/local/eui-core/classic/src/form/Label.js
+++ packages/local/eui-core/classic/src/form/Label.js
@@ -3,5 +3,23 @@ Ext.define('eui.form.Label', {
     alias: 'widget.euilabel',
     cellCls: 'fo-table-row-th',
     allowBlank : true,
-    localeProperties: ['html', 'text']
+    localeProperties: ['html', 'text'],
+    initComponent: function () {
+        var me = this;
+        if(me.allowBlank===false){
+            Ext.apply(me, {
+                cls: 'fo-required'
+            });
+        }
+        me.callParent(arguments);
+
+        /***
+         * form 내부에서 사용시 대상 폼필드 allowBlank설정.
+         */
+        me.on('afterrender', function(){
+            if(me.nextSibling() && me.nextSibling().isFormField ){
+                me.nextSibling().allowBlank = me.allowBlank;
+            }
+        });
+    }
 });
