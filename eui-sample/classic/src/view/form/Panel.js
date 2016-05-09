0f10d984887ee2772795d901bd24c6ecfe5ce4e0
benney <benneykwag@gmail.com>
2016-05-09 23:34:11 +0900

    폼필드 레이블에 백그라운드 컬러 적용되도록 변경하였으므로 euilabel사용하지 않는 쪽으로 샘플을 변경.

--- eui-sample/classic/src/view/form/Panel.js
+++ eui-sample/classic/src/view/form/Panel.js
@@ -24,161 +24,16 @@ Ext.define('Eui.sample.view.form.Panel', {
             flex: 1,
             xtype: 'euiform',
             frame: true,
-            title: 'EUI 폼패널',
-            tableColumns: 4,
-            defaults: {
-                hideLabel: true
-//                cellCls: 'fo-table-row-td-noneborder'
-            },
-            items: [
-                {
-                    xtype: 'euilabel',
-                    text: '회원구분'
-                },
-                {
-                    xtype: 'euiradiogroup',
-                    fieldLabel: '회원구분',
-                    items: [
-                        {
-                            boxLabel: '일반',
-                            checked: true,
-                            inputValue: 'N'
-                        },
-                        {
-                            boxLabel: '법인 관리자',
-                            inputValue: 'S'
-                        }
-                    ],
-                    bind: {
-                        value: '{regMember.memberFlag}'
-                    }
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '아이디'
-                },
-                {
-                    fieldLabel: '아이디',
-                    xtype: 'euitext'
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '비밀번호'
-                },
-                {
-                    fieldLabel: '비밀번호',
-                    xtype: 'euitext'
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '비밀번호 확인'
-                },
-                {
-                    fieldLabel: '비밀번호 확인',
-                    xtype: 'euitext'
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '성명'
-                },
-                {
-                    fieldLabel: '성명',
-                    xtype: 'euitext'
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '이메일'
-                },
-                {
-                    fieldLabel: '이메일',
-                    allowBlank: false,
-                    xtype: 'euitext',
-                    vtype: 'email'
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '연락처'
-                },
-                {
-                    fieldLabel: '연락처',
-                    allowBlank: false,
-                    name: 'phone',
-                    xtype: 'euitext'
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '성별'
-                },
-                {
-                    xtype: 'euiradiogroup',
-                    fieldLabel: '성별',
-                    bind: {
-                        value: '{regMember.gender}'
-                    },
-                    items: [
-                        {
-                            boxLabel: '남성',
-                            inputValue: 'M'
-                        },
-                        {
-                            boxLabel: '여성',
-                            inputValue: 'F'
-                        }
-                    ]
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '체크박스'
-                },
-                {
-                    colspan: 3,
-                    xtype: 'euicheckboxgroup',
-                    fieldLabel: 'Two Columns',
-                    columns: 6,
-                    vertical: true,
-                    bind: {
-                        value: '{regMember.rb}'
-                    },
-                    items: [
-                        { boxLabel: 'Item 1', name: 'rb', inputValue: '1' },
-                        { boxLabel: 'Item 2', name: 'rb', inputValue: '2'},
-                        { boxLabel: 'Item 3', name: 'rb', inputValue: '3' },
-                        { boxLabel: 'Item 4', name: 'rb', inputValue: '4' },
-                        { boxLabel: 'Item 5', name: 'rb', inputValue: '5' },
-                        { boxLabel: 'Item 6', name: 'rb', inputValue: '6' }
-                    ]
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '생년월일'
-                },
-                {
-                    fieldLabel: '생년월일',
-                    xtype: 'euidate'
-                },
-                {
-                    xtype: 'euilabel',
-                    text: '회사'
-                },
-                {
-                    fieldLabel: '회사',
-                    xtype: 'euicombo'
-                }
-            ]
-        },
-        {
-            flex: 1,
-            xtype: 'euiform',
-            frame: true,
-            title: 'EUI 폼패널',
+            title: 'EUI 폼',
             tableColumns: 2,
             defaults: {
                 hideLabel: false,
-                cellCls: 'fo-table-row-td-noneborder'
+                allowBlank: false
             },
             items: [
                 {
                     xtype: 'euiradiogroup',
+                    allowBlank: false,
                     fieldLabel: '회원구분',
                     items: [
                         {
@@ -196,6 +51,11 @@ Ext.define('Eui.sample.view.form.Panel', {
                     }
                 },
                 {
+//                                        fieldStyle: 'background-color:yellow;background-image:none',
+//                    cls: 'custom',
+                    allowBlank: true,
+//                    labelCls: 'td-style',
+//                    labelStyle: 'background-color:yellow;background-image:none',
                     fieldLabel: '아이디',
                     xtype: 'euitext'
                 },
@@ -264,7 +124,17 @@ Ext.define('Eui.sample.view.form.Panel', {
                 },
                 {
                     fieldLabel: '회사',
+                    editable: true,
                     xtype: 'euicombo'
+                }
+            ],
+            buttons: [
+                {
+                    text: '확인',
+                    formBind: true
+                },
+                {
+                    text: '취소'
                 }
             ]
         }
