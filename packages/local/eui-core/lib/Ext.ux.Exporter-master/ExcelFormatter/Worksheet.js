0fcbf3bf0dad09d3d43aeb02994f5a5d3dacd3ba
benney <benneykwag@gmail.com>
2016-11-16 17:38:33 +0900

    Fixed #54

--- packages/local/eui-core/lib/Ext.ux.Exporter-master/ExcelFormatter/Worksheet.js
+++ packages/local/eui-core/lib/Ext.ux.Exporter-master/ExcelFormatter/Worksheet.js
@@ -181,8 +181,10 @@ Ext.define("Ext.ux.exporter.excelFormatter.Worksheet", {
     },
 
     buildCell: function(value, type, style) {
-        if (type == "DateTime" && Ext.isFunction(value.format)) value = value.format(this.dateFormatString);
-
+        if (type == "DateTime" && Ext.isFunction(value.format)){
+            value = value.format(this.dateFormatString);
+        }
+        value = value.replace(/</g,"&lt;");
         return new Ext.ux.exporter.excelFormatter.Cell({
             value: value,
             type: type,
