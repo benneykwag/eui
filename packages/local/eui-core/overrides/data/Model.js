25a7ebf04258ce0c7c43f3eed58cceb31895cac7
benney <benneykwag@gmail.com>
2016-11-19 23:43:13 +0900

    Fixed #57

--- packages/local/eui-core/overrides/data/Model.js
+++ packages/local/eui-core/overrides/data/Model.js
@@ -20,5 +20,96 @@ Ext.define('Override.data.Model', {
             return false;
         }
         return true;
+    },
+
+    /***
+     * 모델 데이터를 출력한다.
+     * checkboxgroup의 오브젝트 키값이 중복 처리되는 것을
+     * 막기 위한 코드로 출발함.
+     * @param options
+     * @returns {{}}
+     */
+    getData: function (options) {
+        var me = this,
+            ret = {},
+            opts = (options === true) ? me._getAssociatedOptions : (options || ret), //cheat
+            data = me.data,
+            associated = opts.associated,
+            changes = opts.changes,
+            critical = changes && opts.critical,
+            content = changes ? me.modified : data,
+            fieldsMap = me.fieldsMap,
+            persist = opts.persist,
+            serialize = opts.serialize,
+            criticalFields, field, n, name, value;
+
+        // DON'T use "opts" from here on...
+
+        // Keep in mind the two legacy use cases:
+        //  - getData() ==> Ext.apply({}, me.data)
+        //  - getData(true) ==> Ext.apply(Ext.apply({}, me.data), me.getAssociatedData())
+
+        if (content) { // when processing only changes, me.modified could be null
+            for (name in content) {
+                value = data[name];
+
+                field = fieldsMap[name];
+                if (field) {
+                    if (persist && !field.persist) {
+                        continue;
+                    }
+                    if (serialize && field.serialize) {
+                        value = field.serialize(value, me);
+                    }
+                }
+                // 기존 코드 ret[name] = value; 를 아래로 대체함.
+                // checkboxgroup 그룹 사용시 아래와 같이 obj가 중복 표현되는 것을 막기 위함..
+                // case 1
+                // job {
+                //    job : ['A01','A02']
+                // }
+                // case 2 (name을 사용하지 않을 경우 처리)
+                // name: 'job',
+                // defaults: {
+                //        name: 'job'
+                //    },
+                var autogenkey ='';
+                if(value instanceof Object){
+                    for(var test in value){
+                        autogenkey = test;
+                    }
+                }
+
+
+                if (value && !Ext.isEmpty(value[name])) {   // case1
+                    ret[name] = value[name];
+                }else if(autogenkey.indexOf('euicheckboxgroup') != -1) { // case2
+                    ret[name] = value[autogenkey];
+                } else {
+                    ret[name] = value;
+                }
+            }
+        }
+
+        if (critical) {
+            criticalFields = me.self.criticalFields || me.getCriticalFields();
+            for (n = criticalFields.length; n-- > 0;) {
+                name = (field = criticalFields[n]).name;
+
+                if (!(name in ret)) {
+                    value = data[name];
+                    if (serialize && field.serialize) {
+                        value = field.serialize(value, me);
+                    }
+                    ret[name] = value;
+                }
+            }
+        }
+
+        if (associated) {
+            me.getAssociatedData(ret, opts); // pass ret so new data is added to our object
+        }
+
+        return ret;
     }
 });
