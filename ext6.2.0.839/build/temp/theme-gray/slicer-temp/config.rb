bab9e7df9fd2c155ff5ba3c86feca77ac9d41009
benney <benneykwag@gmail.com>
2016-11-29 20:34:00 +0900

    sdk ㄹ정리

--- ext6.2.0.839/build/temp/theme-gray/slicer-temp/config.rb
+++
@@ -1,4 +0,0 @@
-
-            require '/Volumes/Disk2/git.projects/eui-workspace/ext/build/classic/theme-gray//config.rb'
-            cache_path = '/Volumes/Disk2/git.projects/eui-workspace/ext/build/.sass-cache'
-        
