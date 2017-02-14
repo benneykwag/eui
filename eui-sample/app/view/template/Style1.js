4aec47ea0447e69ad30d5a65bc27c2ff2eff2833
benney <benneykwag@gmail.com>
2017-02-15 08:37:49 +0900

    샘플 수정.

--- eui-sample/app/view/template/Style1.js
+++ eui-sample/app/view/template/Style1.js
@@ -15,7 +15,7 @@ Ext.define('Eui.sample.view.template.Style1',{
                 '->',
                 {
                     scale: 'medium',
-
+                    reloadBtnICon : 'x-fa fa-download',
                     showPrintBtn: true,
                     showRowAddBtn: true,
                     showRowDelBtn: true,
@@ -48,7 +48,6 @@ Ext.define('Eui.sample.view.template.Style1',{
         {
             xtype : 'euiform',
             margin: 10,
-            iconCls: null,
             title: 'PAGE TITLE 페이지 타이틀',
             header: {
                 xtype: 'header',
@@ -107,33 +106,26 @@ Ext.define('Eui.sample.view.template.Style1',{
                 titlePosition: 0,
                 items: [
                     {
-                        hideTextPrintBtn: true,
-                        hideTextReloadBtn: true,
-                        hideAddBtnICon: true,
-                        hideDelBtnICon: true,
-                        hideRegBtnICon: true,
-                        hideModBtnICon: true,
-                        hideSaveBtnICon: true,
-                        showReloadBtn: true,
+                        printBtnText: '출력버튼',
+                        rowAddBtnText: '추가버튼',
+                        rowDelBtnText: '삭제버튼',
+                        regBtnText: '등록버튼',
+                        reloadBtnText: '조회버튼',
+                        modBtnText: '수정버튼',
+                        saveBtnText: '저장버튼',
+                        closeBtnText: '닫기버튼',
+                        excelDownBtnText: '엑셀다운로드버튼',
+
                         showPrintBtn: true,
                         showRowAddBtn: true,
                         showRowDelBtn: true,
+                        showRegBtn: true,
+                        showReloadBtn: true,
+                        showModBtn: true,
                         showSaveBtn: true,
-                        xtype: 'euicommand',
-                        params: {
-                            PGMID: 'A000',
-                            POSIT: '1'
-                        },
-                        listeners: {
-                            rowaddbtnclick: function () {
-
-                            },
-                            regbtnclick: 'onRowReg',
-                            rowdeletebtnclick: function () {
-
-                            },
-                            savebtnclick: 'onRowSave'
-                        }
+                        showCloseBtn: true,
+                        showExcelDownBtn: true,
+                        xtype: 'euicommand'
                     }
                 ]
             },
